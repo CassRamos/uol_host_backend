76c3dd4e72d4a58b4044c70183b1864f9fc06350
Cass <cassianoleaoramos@gmail.com>
2024-03-24 13:20:53 -0300

    Initial commit

---
+++ src/test/java/cass/uol_host/UolHostApplicationTests.java
@@ -0,0 +1,13 @@
+package cass.uol_host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolHostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
